d517c06aeeb4066721dc3c3b8f5b73221adf69d9
Patrick Robinson <patrick.robinson@envato.com>
2015-08-01 20:23:17 +1000

    Remove redundant code. Account for max of less than 3.

--- lib/primelix.ex
+++ lib/primelix.ex
@@ -1,11 +1,13 @@
 defmodule Primelix do
-  def main(args) do
-    {max, _} = Integer.parse(hd(args))
-    primes_up_to(3, [2], max)
+  def main([ {max, _} | _ ]) do
+    primes_up_to(max)
     |> Enum.map(&IO.puts(&1))
+    :ok
   end
 
-  def primes_up_to(x, primes, max) when max < 2, do: []
+  def primes_up_to(max) when max < 2, do: []
+  def primes_up_to(max) when max == 2, do: [2]
+  def primes_up_to(max), do: primes_up_to(3, [2], max)
 
   def primes_up_to(x, primes, max) when x > max, do: primes
 
@@ -14,10 +16,6 @@ defmodule Primelix do
       primes = primes ++ [x]
     end
     primes_up_to(x + 2, primes, max)
-  end
-
-  def is_prime(x, primes) do
-    !divisible_by_any?(x, primes)
   end
 
   def divisible_by_any?(x, ints) do
